3bca99bce7eea10c813e623ff990073d3014b835
Doron Michlal <razdoron9@gmail.com>
2021-01-20 20:08:24 +0200

    REST CONTROLLER

---
+++ server/controllers/restController.js
@@ -0,0 +1,15 @@
+/**************************************************
+ ************** GET TOP 5 RESTAURANTS *************
+ **************************************************/
+
+// Get all restaurants
+
+// Get restaurant by ID
+
+// Create restaurant
+
+// Update restaurant
+
+// Delete restaurant
+
+// Top 5 re
